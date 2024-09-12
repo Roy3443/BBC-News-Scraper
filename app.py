7906abb4ebdc25e2dc04ae461625a9a6f5a3fdcf
Roy3443 <21b43.roy@sjec.ac.in>
2024-09-12 18:27:01 +0530

    live news and sports

--- app.py
+++ app.py
@@ -15,3 +15,17 @@ def get_headlines(category):
 @app.route('/', methods = ['GET'])
 def home():
     news_headlines = get_headlines('live')
+    return render_template('index.html', news_headlines =news_headlines)
+
+@app.route('/live_news', methods=['GET'])
+def live_news():
+    headlines = get_headlines('live/news')
+    return render_template('live_news.html', headlines=headlines)
+
+@app.route('/live_sports')
+def live_sports():
+    headlines = get_headlines('live/sports')
+    return render_template('live_sports.html', headlines=headlines)
+
+if __name__ == '__main__':
+    app.run(debug=True)
